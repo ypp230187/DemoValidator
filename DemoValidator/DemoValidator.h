fd0dc9abe3fa5a5e432bca0f36ef9b989b3d8e7c
dhacr7 <puput.yudha87@gmail.com>
2024-06-28 23:11:38 +0700

    Initial Commit

---
+++ DemoValidator/DemoValidator.h
@@ -0,0 +1,18 @@
+//
+//  DemoValidator.h
+//  DemoValidator
+//
+//  Created by Yudha Pratama Putra on 28/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoValidator.
+FOUNDATION_EXPORT double DemoValidatorVersionNumber;
+
+//! Project version string for DemoValidator.
+FOUNDATION_EXPORT const unsigned char DemoValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoValidator/PublicHeader.h>
+
+
